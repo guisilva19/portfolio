7e2cd19ba38b67080655a54321cd8f711963efb2
Guilherme Silva <2003silvagui@gmail.com>
2024-03-24 23:58:30 -0300

    fix: correct slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SliderAnimation from "@/components/slider/slider";
 export default function Home() {
   return (
     <>
-      <main className="w-screen h-screen bg-primary-black">
+      <main className="max-w-screen h-screen bg-primary-black">
         <div className="w-screen h-[500px]"></div>
         <div className="position-relative mt-10">
           <SliderAnimation />
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -22,10 +22,10 @@ export default function SliderAnimation() {
   return (
     <>
       <div
-        className={`${styles.textContainer} h-16 w-screen flex items-center bg-white gap-8`}
+        className={`${styles.textContainer} h-16 w-[105vw] flex items-center bg-white rotate-6 md:rotate-3`}
       >
         <div className={`${styles.text} text-3xl`}>
-          <div className="inline-flex gap-8">
+          <div className="inline-flex">
             {texts.map((text, index) => (
               <>
                 <span
@@ -39,12 +39,12 @@ export default function SliderAnimation() {
                   alt="star"
                   width={40}
                   height={40}
-                  className=""
+                  className="mx-8"
                 />
               </>
             ))}
           </div>
-          <div className="inline-flex gap-8">
+          <div className="inline-flex">
             {texts.map((text, index) => (
               <>
                 <span
@@ -58,7 +58,7 @@ export default function SliderAnimation() {
                   alt="star"
                   width={40}
                   height={40}
-                  className=""
+                  className="mx-8"
                 />
               </>
             ))}
@@ -67,10 +67,10 @@ export default function SliderAnimation() {
       </div>
 
       <div
-        className={`${styles.textContainerTwo} h-16 w-screen flex items-center bg-white gap-8`}
+        className={`${styles.textContainerTwo} h-16 w-[105vw] flex items-center bg-white -rotate-6 md:-rotate-3`}
       >
         <div className={`${styles.textTwo} text-3xl`}>
-          <div className="inline-flex gap-8">
+          <div className="inline-flex">
             {texts.map((text, index) => (
               <>
                 <span
@@ -79,11 +79,17 @@ export default function SliderAnimation() {
                 >
                   {text}
                 </span>
-                <Image src={starIcon} alt="star" width={40} height={40} />
+                <Image
+                  src={starIcon}
+                  alt="star"
+                  width={40}
+                  height={40}
+                  className="mx-8"
+                />
               </>
             ))}
           </div>
-          <div className="inline-flex gap-8">
+          <div className="inline-flex">
             {texts.map((text, index) => (
               <>
                 <span
@@ -92,7 +98,13 @@ export default function SliderAnimation() {
                 >
                   {text}
                 </span>
-                <Image src={starIcon} alt="star" width={40} height={40} />
+                <Image
+                  src={starIcon}
+                  alt="star"
+                  width={40}
+                  height={40}
+                  className="mx-8"
+                />
               </>
             ))}
           </div>
