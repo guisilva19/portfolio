a07bf27d1445f708fe27a7cc55fc2a3bf1863233
Guilherme Silva <2003silvagui@gmail.com>
2024-03-28 11:13:06 -0300

    fix: correct import to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/hero/hero";
-import Skils from "@/components/skils/skils";
 
 export default function Home() {
   return (
     <>
       <main className="w-screen h-screen">
         <Hero />
-        <Skils />
       </main>
     </>
   );
