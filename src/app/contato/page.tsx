aeb25fdd2a1620285087fc79c1265e008b1e10e8
Guilherme Silva <2003silvagui@gmail.com>
2024-04-04 16:12:01 -0300

    feat: gap +

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -6,7 +6,7 @@ export default function Contato() {
   return (
     <>
       <NextUIProvider>
-        <main className="w-screen h-full flex flex-col py-10 md:py-20 gap-3">
+        <main className="w-screen h-full flex flex-col py-10 md:py-20 gap-5">
           <Form />
           
         </main>
