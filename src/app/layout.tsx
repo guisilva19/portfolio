eff0f97a8135578e617a1ba2d24465712cd4ca23
Guilherme Silva <2003silvagui@gmail.com>
2024-04-07 16:35:49 -0300

    feat: projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/footer/footer";
 
 const plus = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gui Silva",
+  title: "Portfólio | Gui Silva - Desenvolvedor Full Stack",
   description:
     "O projeto 'Portfólio Multifuncional de Guilherme Silva Fernandes' é uma plataforma digital personalizada desenvolvida para destacar os talentos, habilidades e conquistas de Guilherme de uma forma única e impactante. Este portfólio é projetado para oferecer uma visão abrangente do seu trabalho e personalidade, demonstrando sua expertise e criatividade de maneira cativante.",
   icons: {
---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,12 @@
+export default function Projects() {
+  return (
+    <>
+      <main className="mt-40 h-full flex flex-col justify-center items-center">
+        <h1 className="h-20 bg-blue-500 bg-gradient-to-r from-primary-pink via-primary-orange to-primary-yellow bg-clip-text text-transparent font-black text-3xl md:text-5xl lg:text-6xl">
+          Projetos
+        </h1>
+        <div className="w-full bg-blue-600 h-96"></div>
+      </main>
+    </>
+  );
+}
