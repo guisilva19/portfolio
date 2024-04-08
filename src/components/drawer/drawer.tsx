06678214a6abfa1ca960ade8dc163d0bd9e0228c
Guilherme Silva <2003silvagui@gmail.com>
2024-04-08 15:03:48 -0300

    feat: custom drawer

--- src/components/drawer/drawer.tsx
+++ src/components/drawer/drawer.tsx
@@ -9,10 +9,10 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { Button } from "@nextui-org/react";
 import { X } from "lucide-react";
 import { IProject } from "../projects/data/projectsList";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Drawer({
   children,
@@ -25,17 +25,41 @@ export default function Drawer({
     <>
       <DrawerMain>
         <DrawerTrigger>{children}</DrawerTrigger>
-        <DrawerContent className="bg-primary-black border-primary-black h-96 w-screen">
-          <DrawerHeader className="relative">
+        <DrawerContent className="bg-primary-black border-primary-black h-[500px] w-screen flex flex-col justify-between">
+          <DrawerHeader className="relative flex lg:justify-center">
             <DrawerTitle>{project.title}</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
             <DrawerClose className="absolute right-5 top-3">
               <X />
             </DrawerClose>
           </DrawerHeader>
-          <DrawerFooter className="flex-row justify-center">
-            <Link href={project.link_github}>Acessar projeto</Link>
-            <Link href={project.link_site}>Acessar repositório</Link>
+          <div className=" w-full flex flex-col items-center justify-center gap-2 md:gap-4  lg:gap-8 md:flex-row">
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={300}
+              height={300}
+              className="rounded-lg md:w-[400px] lg:w-[500px]"
+            />
+            <span className="flex flex-col h-full justify-between 2xl:w-[400px]">
+              <DrawerDescription className="text-center w-72 md:text-start md:text-[15px] lg:text-lg ">
+                {project.description}
+              </DrawerDescription>
+              <p>{project.date}</p>
+            </span>
+          </div>
+          <DrawerFooter className="flex-row justify-center mt-4">
+            <Link
+              href={project.link_github}
+              className="flex justify-center items-center text-center text-sm md:text-base bg-gradient-to-r from-primary-pink via-primary-orange to-primary-yellow px-5 py-3 rounded-lg hover:scale-105 transition-[400ms] font-semibold"
+            >
+              Acessar projeto
+            </Link>
+            <Link
+              href={project.link_site}
+              className="flex justify-center items-center text-center text-sm md:text-base bg-gradient-to-r from-primary-pink via-primary-orange to-primary-yellow px-5 py-3 rounded-lg hover:scale-105 transition-[400ms] font-semibold"
+            >
+              Acessar repositório
+            </Link>
           </DrawerFooter>
         </DrawerContent>
       </DrawerMain>
