a956ddb733f6bcc408f06b8a664a2da347949bed
Guilherme Silva <2003silvagui@gmail.com>
2024-04-09 17:17:30 -0300

    feat: chatbot project

--- src/components/projects/data/projectsList.ts
+++ src/components/projects/data/projectsList.ts
@@ -1,6 +1,7 @@
 import { BiLogoTypescript } from "react-icons/bi";
 import { FaReact } from "react-icons/fa6";
-import { SiStyledcomponents } from "react-icons/si";
+import { SiStyledcomponents, SiNextdotjs, SiTailwindcss } from "react-icons/si";
+import { RiOpenaiFill } from "react-icons/ri";
 
 export interface IProject {
   title: string;
@@ -8,30 +9,30 @@ export interface IProject {
   image?: any;
   description: string;
   link_github: string;
-  link_site: string;
+  link_site?: string;
   is_finish: boolean;
   libs: any[];
 }
 
 export const projectsList: IProject[] = [
   {
-    title: "Quick Loan",
-    date: "19 de Março de 2024",
-    image: "https://i.ibb.co/NSq0MSH/image-2.png",
-    description: "Quick Loan é uma iniciativa inovadora voltada para a criação de uma plataforma de empréstimo rápido, com foco na agilidade, transparência e facilidade de acesso aos serviços financeiros.",
-    link_github: "https://github.com/guisilva19/quick-loan",
-    link_site: "https://github.com/guisilva19/quick-loan",
+    title: "Chatbot",
+    date: "08 de Abril de 2024",
+    image: "https://i.ibb.co/3dCCG3N/image.png",
+    description:
+      "Este chatbot utiliza tecnologias avançadas para oferecer uma experiência de conversação inteligente e natural. Através da integração com a API da OpenAI, garantindo interações mais humanizadas e relevantes. Além disso, nossa integração com a Vercel SDK AI nos permite enriquecer ainda mais a interação com funcionalidades específicas, tornando nosso chatbot mais inteligente e capaz de compreender melhor as necessidades dos usuários.",
+    link_github: "https://github.com/guisilva19/chatbot",
     is_finish: false,
-    libs: [BiLogoTypescript, FaReact, SiStyledcomponents],
+    libs: [SiNextdotjs, BiLogoTypescript, RiOpenaiFill, SiTailwindcss],
   },
   {
     title: "Quick Loan",
     date: "19 de Março de 2024",
     image: "https://i.ibb.co/NSq0MSH/image-2.png",
-    description: "Quick Loan é uma iniciativa inovadora voltada para a criação de uma plataforma de empréstimo rápido, com foco na agilidade, transparência e facilidade de acesso aos serviços financeiros.",
+    description:
+      "Quick Loan é uma iniciativa inovadora voltada para a criação de uma plataforma de empréstimo rápido, com foco na agilidade, transparência e facilidade de acesso aos serviços financeiros.",
     link_github: "https://github.com/guisilva19/quick-loan",
-    link_site: "https://github.com/guisilva19/quick-loan",
     is_finish: false,
-    libs: [BiLogoTypescript, FaReact, SiStyledcomponents],
+    libs: [FaReact, BiLogoTypescript, SiStyledcomponents],
   },
 ];
